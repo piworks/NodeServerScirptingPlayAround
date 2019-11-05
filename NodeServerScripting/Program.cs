71606dda0bc085f04de420355b875c274190c0a6
ahmet.urun <ahmet.urun@outllook.com>
2019-11-05 09:48:19 +0300

    initial commit for node server scripting playaround repo

---
+++ NodeServerScripting/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace NodeServerScripting
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var process = new Process())
+			{
+				process.StartInfo = new ProcessStartInfo
+				{
+					WindowStyle = ProcessWindowStyle.Hidden,
+					CreateNoWindow = true,
+					UseShellExecute = false,
+					RedirectStandardOutput = true,
+					RedirectStandardError = true,
+					FileName = AppContext.BaseDirectory + "node.exe",
+					Arguments = "server.js"
+				};
+				process.Start();
+
+				Task.Delay(30000).ConfigureAwait(false).GetAwaiter().GetResult();
+				process.Kill();
+			}
+		}
+	}
+}
